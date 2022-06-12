84c5a48e84be802878d2f1bd16ed7f7767b6afc8
JudiciousX <1119833684@qq.com>
2022-06-12 18:44:18 +0800

    ViewModel

--- app/src/main/java/com/example/viewmodel/MyViewModel.kt
+++ app/src/main/java/com/example/viewmodel/MyViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 //ViewModel 的生命周期与activity不同 ，它可以保证手机在横竖屏切换时不被重新创建
 //只有当Activity退出时 它才会被销毁
-class MyViewModel : ViewModel() {
+class MyViewModel(application: Application) : AndroidViewModel(application) {
     var number = 0
 }
